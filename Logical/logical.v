0270a087f4deeee97132fab49a308d3a3a9ca49d
Andrew Zhu <36798911+andrewyzhu@users.noreply.github.com>
2018-03-02 23:43:17 -0700

    Add files via upload

--- Logical/logical.v
+++ Logical/logical.v
@@ -1,34 +1,3 @@
-module logical(x,y,z,out1,out2,out3,rout);
-	input [3:0] x; 
-	input [3:0] y;
-	input [7:0] z;
-	input [1:0] s;
-	output reg [7:0] rout;
-	output reg [3:0] out1,out2,out3;
-	wire [3:0]d,e,f;
-	wire [7:0] g;
-	
-	aAND D1(x,y,d);
-	oOR E1(x,y,e);
-	EXOR F1(x,y,f);
-	nNOR G1(z,g);
-	
-	assign out1 = d;
-	assign out2 = e;
-	assign out3 = f;
-	assign rout = g;
-	/*always@(x,y,z)
-	begin
-		case(s)
-		2'b00 : out2 = d;
-		2'b01 : out2 = e;
-		2'b10 : out2 = f;
-		2'b11 : rout = g;
-		endcase
-	end	
-	*/
-endmodule 
-
 module aAND(x,y,aout);
 	input [3:0] x;
 	input [3:0] y;
