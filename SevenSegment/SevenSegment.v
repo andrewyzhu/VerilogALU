8c552fea867d76bde23bb4e606c5a9a281d7c617
Andrew Zhu <36798911+andrewyzhu@users.noreply.github.com>
2018-03-02 23:42:24 -0700

    Add files via upload

--- SevenSegment/SevenSegment.v
+++ SevenSegment/SevenSegment.v
@@ -6,23 +6,22 @@ module sevensegment(binaryin,decin,sevenseg,decout);
 	assign decout = decin;
 	always @(binaryin) begin
 		case(binaryin)
-			4'b0000 : sevenseg = 7'b0000001;
-			4'b0001 : sevenseg = 7'b1001111;
-			4'b0010 : sevenseg = 7'b0010010;
-			4'b0011 : sevenseg = 7'b0000110;
-			4'b0100 : sevenseg = 7'b1001100;
-			4'b0101 : sevenseg = 7'b0100100;
-			4'b0110 : sevenseg = 7'b0100000;
-			4'b0111 : sevenseg = 7'b0001111;
-			4'b0100 : sevenseg = 7'b1001100;
-			4'b1000 : sevenseg = 7'b0000000;
-			4'b1001 : sevenseg = 7'b0000100;
-			4'b1010 : sevenseg = 7'b0001000;
-			4'b1011 : sevenseg = 7'b1100000;
-			4'b1100 : sevenseg = 7'b1110001;
-			4'b1101 : sevenseg = 7'b1000010;
-			4'b1110 : sevenseg = 7'b0110000;
-			4'b1111 : sevenseg = 7'b0111000;
+			4'b0000 : sevenseg = 7'b1000000; //0
+			4'b0001 : sevenseg = 7'b1111001; //1
+			4'b0010 : sevenseg = 7'b0100100;	//2
+			4'b0011 : sevenseg = 7'b0110000;	//3
+			4'b0100 : sevenseg = 7'b0011001;	//4
+			4'b0101 : sevenseg = 7'b0010010;	//5
+			4'b0110 : sevenseg = 7'b0000010;	//6
+			4'b0111 : sevenseg = 7'b1111000;	//7
+			4'b1000 : sevenseg = 7'b0000000;	//8
+			4'b1001 : sevenseg = 7'b0010000;	//9
+			4'b1010 : sevenseg = 7'b0001000;	//A
+			4'b1011 : sevenseg = 7'b0000011;	//b
+			4'b1100 : sevenseg = 7'b1000110;	//C
+			4'b1101 : sevenseg = 7'b0100001;	//d
+			4'b1110 : sevenseg = 7'b0000110; //E
+			4'b1111 : sevenseg = 7'b0001110;	//f
 			endcase
 		end
 endmodule 
