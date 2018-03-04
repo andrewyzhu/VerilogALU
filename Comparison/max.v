6de1f3837bd89f4ccc8372ae3f7b1258c8c3ad06
Kurt Sprague <34193116+kurtsprague@users.noreply.github.com>
2018-03-04 15:59:10 -0700

    Add files via upload

--- Comparison/max.v
+++ Comparison/max.v
@@ -1,3 +1,13 @@
+// ============================================================
+//   Ver  :| Authors					               :| Mod. Date :|
+//   V1.1 :| Andrew Zhu and Kurt Sprague			:| 3/4/2018  :|
+// ============================================================
+// module max that takes in 2 inputs and outputs the maximum of the two
+// output is going to be a wire
+// wire the output of our greater than module 
+// use our greater than module to do a bitwise AND between every bit of the input and the output of our greater than module
+// bitwise OR the result
+// assign each bit of the output to each individual result
 module max(x,y,o);
 	input [3:0] x;
 	input [3:0] y;
