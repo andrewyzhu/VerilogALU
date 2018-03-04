36df51ce00dfca68982d4e2a46b3665fc3cea31e
Kurt Sprague <34193116+kurtsprague@users.noreply.github.com>
2018-03-04 16:20:47 -0700

    Add files via upload

--- Arithmetic/fulladder.v
+++ Arithmetic/fulladder.v
@@ -1,3 +1,12 @@
+// ============================================================
+//   Ver  :| Authors					               :| Mod. Date :|
+//   V1.1 :| Andrew Zhu and Kurt Sprague			:| 3/4/2018  :|
+// ============================================================
+// module to operate as a full adder with
+// 2 numbers, a carry in
+// and output a sum and a carry out
+// a full adder works as a sum equaling the XOR of its inputs
+// the carry out is an OR of each input AND
 module fulladder(x,y,cin,s,cout);
 	input x;
 	input y;
--- Arithmetic/fullsubtractor.v
+++ Arithmetic/fullsubtractor.v
@@ -1,3 +1,12 @@
+// ============================================================
+//   Ver  :| Authors					               :| Mod. Date :|
+//   V1.1 :| Andrew Zhu and Kurt Sprague			:| 3/4/2018  :|
+// ============================================================
+// module to act as a full subtractor, similar to a full adder
+// the inputs are similar because they are 2 numbers and a borrow in
+// the out puts are similar as they are a difference and a borrow out
+// the difference is similar in the way that they are an XOR of the inputs
+// but the borrow out is different in the way that the number you are subtracting must be inverted in the borrow out logic
 module fullsubtractor(x,y,bin,d,bout);
 	input x;
 	input y;
