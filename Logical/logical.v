ba0392845fd96e9d4c6c89e8d21d35839a1a6769
Kurt Sprague <34193116+kurtsprague@users.noreply.github.com>
2018-03-04 16:39:04 -0700

    Add files via upload

--- Logical/logical.v
+++ Logical/logical.v
@@ -1,13 +1,9 @@
-module aAND(x,y,aout);
-	input [3:0] x;
-	input [3:0] y;
-	output [3:0] aout;
-	
-	assign aout = x&y;
-	
-endmodule 
+// ============================================================
+//   Ver  :| Authors					               :| Mod. Date :|
+//   V1.1 :| Andrew Zhu and Kurt Sprague			:| 3/4/2018  :|
+// ============================================================
 
-
+//assign statement doing the bitwise or of the input
 module oOR(x,y, oout);
 	input [3:0] x;
 	input [3:0] y;
@@ -17,6 +13,15 @@ module oOR(x,y, oout);
 	
 endmodule 
 
+//assign statement doing the bitwise NOT of the input
+module nNOT(z,nout);
+	input [7:0]z;
+	output [7:0] nout;
+	
+	assign nout = ~z;
+endmodule 
+
+// assign statement in a module that outputs the bitwise EXOR of the input
 module EXOR(x,y,eout);
 	input [3:0] x;
 	input [3:0] y;
@@ -25,9 +30,12 @@ module EXOR(x,y,eout);
 	assign eout = x^y;
 endmodule 
 
-module nNOT(z,nout);
-	input [7:0]z;
-	output [7:0] nout;
+//assign statement in a module that outputs a bitwise and of the inputs
+module aAND(x,y,aout);
+	input [3:0] x;
+	input [3:0] y;
+	output [3:0] aout;
 	
-	assign nout = ~z;
+	assign aout = x&y;
+	
 endmodule 
