33e4bfcdb43b1191cd2eb6a3c7eefac927beab50
Andrew Zhu <36798911+andrewyzhu@users.noreply.github.com>
2018-03-04 02:55:15 -0700

    Add files via upload

--- knightrider.v
+++ knightrider.v
@@ -1,42 +1,32 @@
-module knightrider(clk,out);
-	input clk;
-	output[9:0] out;
-
-	reg clkshift;
-	
-	reg [13:0] totalled; //2 extra leds that dont exist on each side of the 10
-	reg [1:0] currentpos; // 2 leds at a time scrolling
-	
-
-	always@(posedge clk)
-		begin
-			if (clkshift <= 500)
-				begin
-					clkshift = clkshift + 1;
-				end
-			else
-				begin
-					clkshift = 0;
-				end
+module knightrider(clkin,LEDout);
+	input clkin;
+	reg [19:0] divider;
+	reg shiftdirection;
+	output reg [9:0] LEDout;
+	reg [13:0] LEDbuf; 	 
+	initial begin
+		divider = 0;
+		shiftdirection = 0;
+		LEDbuf = 14'b00000000001100;
+	end 
+	always @(posedge clkin) begin
+		divider = divider + 1;
+		if(divider == 1000000) begin
+			divider = 0;
+			if(LEDbuf[13:12] == 2'b11) begin
+				shiftdirection = 1;
+			end
+			if(LEDbuf[1:0] == 2'b11) begin
+				shiftdirection = 0;
+			end
+			if(shiftdirection == 0) begin
+				LEDbuf = LEDbuf << 1;
+			end
+			if(shiftdirection == 1) begin
+				LEDbuf = LEDbuf >> 1;
+			end
+			LEDout = LEDbuf [11:2];
 		end
-	
-	assign delayedclk = clkshift; //delayed clock
-	
-
-	
-	always@(delayedclk) //shift
-		begin
-			if (currentpos == 0)
-				begin
-					totalled <= totalled << 1;
-				end
-			else 
-				begin
-					totalled <= totalled >> 1;
-				end
-			currentpos <= currentpos + 1;
-		end
-	
-	
-	assign out [8:0] = totalled [10:2]; // make sure the 10 leds output are 10 of the fake array
+	end
 endmodule
+			
