409eacdda8cb13ca59eae86ae1b43dcc41b9799e
Andrew Zhu <36798911+andrewyzhu@users.noreply.github.com>
2018-03-04 19:59:28 -0700

    Update signedadder.v

--- signedadder.v
+++ signedadder.v
@@ -3,23 +3,34 @@
 //   V1.1 :| Andrew Zhu and Kurt Sprague			:| 3/4/2018  :|
 //   Digital Logic ECEN 2350 Spring Semester
 // ============================================================
+
+// signed adder module
+// takes in two 4 bit signed numbers
+// sign extends both inputs by 1 bit to prevent overflow
+// uses fulladders to add the sign extended inputs
+// sign extends the output to 8 bits
 module signedadder(x,y,s);
-	parameter size = 4;
-	input [size-1:0] x, y;
+	input [3:0] x, y;
 	output [7:0] s;
-	wire [size:0] signextensionx, signextensiony;
-	assign signextensionx [size] = x [size-1];
-	assign signextensionx [size-1:0] = x;
-	assign signextensiony [size] = y [size-1];
-	assign signextensiony [size-1:0] = y;
-	wire [size:0] coutbuf;
+	
+	//sign extension to prevent overflow, msb of sign extension is equal to msb of the input
+	wire [4:0] signextensionx, signextensiony;
+	assign signextensionx [4] = x [3];
+	assign signextensionx [3:0] = x;
+	assign signextensiony [4] = y [3];
+	assign signextensiony [3:0] = y;
+	
+	//same implementation as unsigned adder
+	wire [4:0] coutbuf;
 	fulladder FA1(signextensionx[0],signextensiony[0],0,s[0],coutbuf[0]);
 	genvar i; 
 	generate
-	for(i = 1; i<=size; i=i+1) begin: signedadders
+	for(i = 1; i<=4; i=i+1) begin: signedadders
 		fulladder FA(signextensionx[i],signextensiony[i],coutbuf[i-1],s[i],coutbuf[i]);
 		end
 	endgenerate 
+	
+	//sign extends the output to 8 bits
 	assign s[5] = s[4];
 	assign s[6] = s[4];
 	assign s[7] = s[4];
